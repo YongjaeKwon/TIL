9adf7a044ac9fefd4d99d39e494462a0538c4ab1
권용재 <yongjae116@gmail.com>
2022-08-22 22:51:30 +0900

    0822_BOJ

--- BOJ/1157_단어공부.py
+++ BOJ/1157_단어공부.py
@@ -1,7 +1,14 @@
-string = list(input())
+words = input().upper()
+unique_word = list(set(words))
 
-max_c = 0
-for i in string:
-    count = 0
-    if i in string:
-        count +=1 
+count_words = []
+
+for i in unique_word:
+    count = words.count(i)
+    count_words.append(count)
+
+if count_words.count(max(count_words)) > 1:
+    print('?')
+else:
+    max_index = count_words.index(max(count_words))
+    print(unique_word[max_index])
